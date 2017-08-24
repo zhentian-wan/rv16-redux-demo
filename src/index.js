106d5572c098a501368f638af54a3b8629beed92
Zhentian Wan <answer881215@gmail.com>
2017-08-24 20:10:16 +0300

    create store

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store';
 
-const state = {
-    todos: [
-        {id: 1, name: 'Render static UI', isComplete: true},
-        {id: 2, name: 'Create initial state', isComplete: true},
-        {id: 3, name: 'Render based on state', isComplete: true}
-    ]
-};
+function render() {
+    const state = store.getState();
+    ReactDOM.render(<App {...state}/>, document.getElementById('root'));
+}
+render();
+store.subscribe(render);
 
-ReactDOM.render(<App todos={state.todos}/>, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/store.js
@@ -0,0 +1,15 @@
+
+import {createStore} from 'redux';
+import reducer from './reducers/todo';
+
+const initState = {
+    todos: [
+        {id: 1, name: 'Render static UI', isComplete: true},
+        {id: 2, name: 'Create initial state', isComplete: false},
+        {id: 3, name: 'Render based on state', isComplete: true}
+    ]
+};
+
+const store = createStore(reducer, initState);
+
+export default store;
